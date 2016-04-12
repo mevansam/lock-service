bb1ffccc8eaca7080d3855559b7f868bffc6757c
Mevan Samaratunga <mevansam@gmail.com>
2016-04-12 13:55:32 +0400

    enabled debug logging of lock test

--- sharedlock/src/test/java/org/appbricks/commons/sharedlock/LockTestContext.java
+++ sharedlock/src/test/java/org/appbricks/commons/sharedlock/LockTestContext.java
@@ -69,6 +69,7 @@ public class LockTestContext {
         Properties properties = new Properties();
         properties.setProperty("hibernate.hbm2ddl.auto", "create");
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        properties.setProperty("hibernate.show_sql", "true");
         return properties;
     }
 }
